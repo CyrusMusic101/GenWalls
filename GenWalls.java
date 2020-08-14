27f165bc1d7ff2cb970f01d8b1b166f76d3f8d83
CyrusMusic101 <31782334+CyrusMusic101@users.noreply.github.com>
2020-08-14 12:35:56 -0700

    Update GenWalls.java

--- GenWalls.java
+++ GenWalls.java
@@ -1,1 +1,19 @@
 
+public class GenWalls extends JavaPlugin {
+
+    public void onEnable() {
+      instance = this 
+      Bukkit.getConsoleSender.sendMessage(ColotUtils.color("&d-=-=-=-=-=-=-=-=-=-=-"));
+      Bukkit.getConsoleSender.sendMessage(ColorUtils.color("&aGenWalls Enabled made));
+      Bukkit.getConsoleSender.SendMessage(ColorUtils.color("&fMade by Octobertroy &cOwned by Berserk Network"));
+      Bukkit.getConsoleSender.sendMessage(ColorUtils.color("&d-===================-));                                      
+                                                           
+    }
+  
+  
+  
+    public void onDisable() {
+      Bukkit.getConsoleSender.sendMessage(ColorUtils.color("&4GenWalls disabled));
+    }
+  
+}
